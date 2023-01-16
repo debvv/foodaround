15d3d77b601bad9710257bd533c0c503b16ef694
Evgeniy <jenea983@mail.ru>
2023-01-16 16:58:45 +0200

    swap

--- order_table.php
+++ order_table.php
@@ -12,19 +12,22 @@
       <br> <br> <br> <br> <br> 
       <?php
       // $dbh = new PDO('mysql:dbname=chatapp; host=localhost', 'root', '');
-      // $sth = $dbh->prepare("SELECT * FROM `orders` WHERE accepted=1233085864 "); //def: select * from orders
+      // $uid = mysqli_real_escape_string($conn, $_GET['unique_id']);
+      // // $sth = $dbh->prepare("SELECT * FROM `orders` WHERE accepted=1233085864 "); //def: select * from orders
+      // $sth = $dbh->prepare("SELECT * FROM `orders` WHERE accepted=1233085864 ");
       // $sth->execute();
       // $list = $sth->fetchAll(PDO::FETCH_ASSOC);
 
       if(!isset($_SESSION['unique_id'])){
         header("location: index.php");
       }
-      $sql = mysqli_query($conn, "SELECT * FROM orders WHERE accepted={$_SESSION['unique_id']}");  // принятые заказы
+      $sql = mysqli_query($conn, "SELECT * FROM orders WHERE accepted=0");// непринятые заказы
+
       ?>
 
       <div class="row">
           <div class="col-sm-6 col-sm-offset-3">
-              <h2 class="module-title font-alt">Orders</h2>
+              <h2 class="module-title font-alt">history Orders culinary</h2>
                 <div class="module-subtitle font-serif"></div>
           </div>
       </div>
@@ -61,17 +64,20 @@
             <td><?php echo $row['name_cul']; ?></td>
             <!-- <td><php echo $row['accepted']; ?></td> -->
             <td>
-							<a class='btn btn-primary btn-sm' href="chat.php?unique_id=<?php echo $row['from_id']; ?>"> text </a>
+              <a class='btn btn-success btn-sm accept' href="assets/php/accept_order.php?id_order=<?php echo $row['id_order']; ?>"> Accept</a>
             </td>
           </tr>
           <?php endforeach; ?>    
         </tbody>
       </table>
-    <br> <br> <br> <br> <br> <br> <br> <br> <br> <br><br> <br>
+<br> <br> <br> <br> <br> <br> <br> <br> <br> <br><br> <br>
 
+     
     <?php include_once "includes/footer.html"; ?> 
+ 
     </main>
+    
       <?php include_once "includes/scripts.html"; ?>   
-      <script async="" defer="" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDK2Axt8xiFYMBMDwwG1XzBQvEbYpzCvFU"></script>       
-    </body>
+      <script async="" defer="" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDK2Axt8xiFYMBMDwwG1XzBQvEbYpzCvFU"></script> 
+  </body>
 </html>
