97159c0a8e5f79d22a2580db623957698fb106d2
Evgeniy <jenea983@mail.ru>
2023-01-16 20:51:15 +0200

    new dumb-db

--- chatapp.sql
+++ chatapp.sql
@@ -3,14 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Янв 12 2023 г., 20:27
--- Версия сервера: 10.4.25-MariaDB
--- Версия PHP: 8.1.10
+-- Время создания: Янв 16 2023 г., 19:50
+-- Версия сервера: 10.4.24-MariaDB
+-- Версия PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -110,7 +109,9 @@ INSERT INTO `messages` (`msg_id`, `incoming_msg_id`, `outgoing_msg_id`, `msg`) V
 (47, 993879515, 1527932594, 'ky jenek'),
 (48, 855284828, 1527932594, 'ei ti'),
 (49, 855284828, 1527932594, 'Маркетплейс (англ. online marketplace, online e-commerce marketplace) — платформа электронной коммерции, интернет-магазин электронной торговли, предоставляющий информацию о продукте или услуге третьих лиц.  В целом маркетплейс представляет собой оптимизированную онлайн-платформу по предоставлению продуктов и услуг. Один и тот же товар зачастую можно купить у нескольких продавцов, при этом цена на товар может различаться.  Поскольку маркетплейсы объединяют продукты от широкого круга поставщиков, выбор этих продуктов более широк, а доступность - выше, чем в специализированных розничных онлайн-магазинах[1]. Начиная с 2014 года число маркетплейсов в глобальной сети Интернет быстро растёт вслед за ростом их востребованности[2]. В 1995 году была основана компания Amazon, которая начинала свою деятельность как онлайновый ритейлер книг; на сегодня она выступает крупнейшим маркетплейсом с восемью филиалами в разных странах мира!!.'),
-(50, 1525781292, 767869359, 'привет ');
+(50, 1525781292, 767869359, 'привет '),
+(51, 767869359, 1527932594, 'привет алекс как там дела с заказом?'),
+(52, 1527932594, 767869359, 'работаем грамотно');
 
 -- --------------------------------------------------------
 
@@ -141,9 +142,10 @@ INSERT INTO `orders` (`id_order`, `time_date`, `name`, `from_id`, `product`, `de
 (4, '0000-00-00 00:00:00', 'Vladislav', 1527932594, 'яблоки', ' ( в кг )', 6, 'Vladik@mail.ru', 'sarmizegetusa street', 'None, you can accept this task', 0),
 (5, '0000-00-00 00:00:00', 'evgen', 1527932594, 'пирожочки', 'с мясом', 4, 'eugeniu.casian@iis.utm.md', 'decebal street', 'None, you can accept this task', 0),
 (6, '2022-11-26 12:44:00', 'Artur', 1527932594, 'пирожочки', 'с мясом', 45, 'eugeniu.casian@iis.utm.md', 'decebal street', 'None, you can accept this task', 0),
-(7, '2023-01-06 08:15:00', 'алекс', 767869359, 'лазанья', 'классическая лазания', 1, 'alex@mail.ru', 'chisinau bd. moscova 21', 'None, you can accept this task', 0),
+(7, '2023-01-06 08:15:00', 'алекс', 767869359, 'лазанья', 'классическая лазания', 1, 'alex@mail.ru', 'chisinau bd. moscova 21', 'None, you can accept this task', 1527932594),
 (9, '2023-01-11 08:00:00', 'alex', 767869359, 'ктлетки', 'из свиноговяжего фарша', 2, 'alex@mail.ru', 'burebista 12', 'None, you can accept this task', 0),
-(10, '2023-01-13 15:00:00', 'alex', 767869359, 'пирожки', 'с кортошкой', 5, 'alex@mail.ru', 'burebista 12', 'None, you can accept this task', 1525781292);
+(10, '2023-01-13 15:00:00', 'alex', 767869359, 'пирожки', 'с кортошкой', 5, 'alex@mail.ru', 'burebista 12', 'None, you can accept this task', 1525781292),
+(11, '2023-01-19 15:32:00', 'admin', 1527932594, 'пирожочки', 'с мясом', 6, 'jenea983@mail.ru', 'decebal street', 'None, you can accept this task', 0);
 
 -- --------------------------------------------------------
 
@@ -174,13 +176,13 @@ INSERT INTO `users` (`unique_id`, `fname`, `lname`, `email`, `password`, `img`, 
 (399259119, 'last', 'Name', 'rootlast@mail.ru', '63a9f0ea7bb98050796b649e85481845', '1667835817AgWyCZmybdE.jpg', 'Offline now', 'Consumer'),
 (496895890, 'Artur', 'Constantinov', 'artur@utm.md', '38f869097037702b56e05f2a6e8c1b7c', '1664800724artur.jpg', 'Offline now', 'Consumer'),
 (742943464, 'dfdf', 'dfdf', 'rodot@mail.ru', '322f25e55eef7948407fa5a30fdbaa99', '1668849157Thic3t6agjA.jpg', 'Offline now', 'Consumer'),
-(767869359, 'alex', 'smith', 'alex@mail.ru', '534b44a19bf18d20b71ecc4eb77c572f', '1673283924Screenshot_3.png', 'Active now', 'Consumer'),
+(767869359, 'alex', 'smith', 'alex@mail.ru', '534b44a19bf18d20b71ecc4eb77c572f', '1673283924Screenshot_3.png', 'Offline now', 'Consumer'),
 (855284828, 'qq', 'qwqqqqqqqqq', 'rooqt@mail.ru', '63a9f0ea7bb98050796b649e85481845', '1667837095Расписанка.jpg', 'Offline now', 'Consumer'),
 (882167199, 'Shef Povar', 'ХЕНК АНДЕРСОН', 'ARAM@mail.ru', 'cf8a270cd13c0d805ac64df6b220df7c', '1664801297Konnor (4).jpg', 'Offline now', 'Culinary Specialist'),
 (993879515, 'evgen', 'casian', 'jenea983@mail.ru', 'e10adc3949ba59abbe56e057f20f883e', '1663691723AgWyCZmybdE.jpg', 'Offline now', 'Consumer'),
 (1233085864, 'pop', 'zvezda', 'jenea9833@gmail.com', 'd6d269952320c4fb5e50f278c94a098c', '16636918946LAFE-wo9Po.jpg', 'Offline now', 'Admin'),
 (1525781292, 'root', 'admin', 'test@mail.ru', 'd8578edf8458ce06fbc5bb76a58c5ca4', '1673282594imgonline-com-ua-Resize-CCE32xRJB6583U.jpg', 'Active now', 'Admin'),
-(1527932594, 'root', 'root', 'root@mail.ru', '63a9f0ea7bb98050796b649e85481845', '1664863729wPvAvnMAmsk.jpg', 'Offline now', 'Admin');
+(1527932594, 'root', 'root', 'root@mail.ru', '63a9f0ea7bb98050796b649e85481845', '1664863729wPvAvnMAmsk.jpg', 'Active now', 'Admin');
 
 --
 -- Индексы сохранённых таблиц
@@ -228,13 +230,13 @@ ALTER TABLE `feedback`
 -- AUTO_INCREMENT для таблицы `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
 
 --
 -- AUTO_INCREMENT для таблицы `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -252,13 +254,6 @@ ALTER TABLE `feedback`
 ALTER TABLE `messages`
   ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`incoming_msg_id`) REFERENCES `users` (`unique_id`),
   ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`outgoing_msg_id`) REFERENCES `users` (`unique_id`);
-
---
--- Ограничения внешнего ключа таблицы `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`from_id`) REFERENCES `users` (`unique_id`),
-  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`accepted`) REFERENCES `users` (`unique_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
