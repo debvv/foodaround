5926934d3bc4181a6be5e0dec35333d6355ac2ca
Evgeniy <jenea983@mail.ru>
2023-05-12 02:42:24 +0300

    Translate to RU, needed

--- index.php
+++ index.php
@@ -13,18 +13,18 @@
         <section class="bg-dark-30 showcase-page-header module parallax-bg" data-background="./assets/images/index/Front.jpg">
         <div class="titan-caption">
           <div class="caption-content">
-            <div class="font-alt mb-30 titan-title-size-1">automatic reception system | food delivery</div>
+            <div class="font-alt mb-30 titan-title-size-1">автоматическая система приёма и передачи заказа кулинарному эксперту | осуществление онлайн заказов </div>
             <?php
             if(!isset($_SESSION['unique_id']))
             {
             ?>
-            <div class="font-alt mb-40 titan-title-size-4">Order safely</div><a class="section-scroll btn btn-border-w btn-round" href="#"> choose a dish </a>
+            <div class="font-alt mb-40 titan-title-size-4">ЗАКАЗЫВАЙТЕ БЕЗОПАСНО</div><a class="section-scroll btn btn-border-w btn-round" href="#"> выбрать блюдо </a>
             <?php
             }
             else
             {
             ?>
-            <div class="font-alt mb-40 titan-title-size-4">Order safely</div><a class="section-scroll btn btn-border-w btn-round" href="make_order.php"> choose a dish </a>
+            <div class="font-alt mb-40 titan-title-size-4">ЗАКАЗЫВАЙТЕ БЕЗОПАСНО</div><a class="section-scroll btn btn-border-w btn-round" href="make_order.php"> выбрать блюдо </a>
             <?php
             }
             ?>            
@@ -42,8 +42,8 @@
             <div class="row">
               <div class="col-sm-6 col-md-8 col-lg-9">
                 <div class="callout-text font-alt">
-                  <h4 style="margin-top: 0px; font-;">Start working in the food industry</h4>
-                  <p style="margin-bottom: 0px;">Or order dishes from the best chefs</p>
+                  <h4 style="margin-top: 0px; font-;">НАЧНИТЕ РАБОТАТЬ В ПИЩЕВОЙ ПРОМЫШЛЕННОСТИ</h4>
+                  <p style="margin-bottom: 0px;">ЛИБО ЗАКАЖИТЕ БЛЮДА ОТ ЛУЧШИХ ШЕФ-ПОВАРОВ</p>
                 </div>
               </div>
               <div class="col-sm-6 col-md-4 col-lg-3">
