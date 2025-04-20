b325b6cede4efe1942a8abc2399d3a6b0b5e2290
debvv <jenea9833@gmail.com>
2025-04-20 19:59:42 +0300

    Fix login

--- assets/php/login.php
+++ assets/php/login.php
@@ -6,15 +6,17 @@
     if(!empty($email) && !empty($password)){
         //проверка введенного юзером маила и пассворда по совпадению в бд с таблицей
         $sql = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
-        if(mysqli_num_rows($sql) > 0){ //если учетные данные юзера совпадают
+        if(mysqli_num_rows($sql) > 0){ 
+            //если учетные данные юзера совпадают
             $row = mysqli_fetch_assoc($sql);
             $user_pass = md5($password);
-            $enc_pass = $row['password'];
+            $enc_pass = $row['password_hash'];
             if($user_pass === $enc_pass){
                 $status = "Active now";
                 $sql2 = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id = {$row['unique_id']}");
                 if($sql2){
-                    $_SESSION['unique_id'] = $row['unique_id']; //используя данную сессию мы используем юникИД в друг пхп файлс
+                    $_SESSION['unique_id'] = $row['unique_id'];
+                     //используя данную сессию мы используем юникИД в друг пхп файлс
                     echo "success";
                 }else{
                     echo "Something went wrong. Please try again!";
