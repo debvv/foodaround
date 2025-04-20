a2845c8b12f6639aa9a5cf22a7b059047d2bde12
debvv <jenea9833@gmail.com>
2025-04-20 16:50:30 +0300

    Fix infinity load: Unknown database name

--- assets/php/config.php
+++ assets/php/config.php
@@ -2,7 +2,7 @@
   $hostname = "localhost";
   $username = "root";
   $password = "";
-  $dbname = "chatapp";
+  $dbname = "foodaround_db";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
   if(!$conn){
